13cbd7c5da2a2f2b45bab88d1421ea46c836aadc
AgungDH <agungdh@live.com>
2025-12-06 17:52:40 +0700

    init

---
+++ src/main/java/id/my/agungdh/bpkad_pajak/BpkadPajakApplication.java
@@ -0,0 +1,13 @@
+package id.my.agungdh.bpkad_pajak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BpkadPajakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BpkadPajakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/my/agungdh/bpkad_pajak/BpkadPajakApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.agungdh.bpkad_pajak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BpkadPajakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
